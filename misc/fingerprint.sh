3c0b670edff7a993dabb7f5f688f5e2ae51526c1
Oliver Kaufmann <kaufmann@novu.ch>
2020-08-26 20:09:31 +0200

    improve fingerprint setup for sudo commands

--- misc/fingerprint.sh
+++ misc/fingerprint.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 sudo chmod +w /etc/pam.d/sudo
+
+echo 'Copy the following to your clipboard and ensure it''s the first line (add it if its missing)'
+echo
+echo 'auth       sufficient     pam_tid.so'
+echo
+echo 'Vim open the config file if you press [ENTER]'
+read
+
 sudo vim /etc/pam.d/sudo
 
-echo 'ensure the following line'
-echo 'auth       sufficient     pam_tid.so'
+
 
 sudo chmod -w /etc/pam.d/sudo
